ad804e7b436476503851384b32d33d9963e368c6
Nihaal Singh <78886474+Nihaal-Singh05@users.noreply.github.com>
2021-06-03 12:49:10 +1200

    Update v3_end_of_quiz_summary.py

--- v3_end_of_quiz_summary.py
+++ v3_end_of_quiz_summary.py
@@ -1,6 +1,7 @@
 #this program is the users end of quiz summary 
 
-r=int(input())
-score=score/rounds
-percentage=score*100
-print(percentage)
+print("\nCongratulations {}!".format(name))
+print("Your Final Score Is",score)
+print("Thanks For Playing : ")
+
+
