a28abce5bbf1ce8827d098e5f431cd63c2f55f75
Nihaal Singh <78886474+Nihaal-Singh05@users.noreply.github.com>
2021-07-26 10:36:55 +1200

    Add files via upload

---
+++ V1/v1user_details.py
@@ -0,0 +1,7 @@
+#this is a simple quiz program
+
+#ask user for name
+name = input("Enter Your Name : ")
+
+#ask user for age
+age = int(input("Enter Your Age : "))
---
+++ V2/v2end_of_quiz_summary.py
@@ -0,0 +1,6 @@
+#this program is on the end of quiz summary for v2
+
+    print("You got", correct, "questions correct")
+    print("You got", incorrect,"Questions incorrect")
+    print("Thank you for playinig this general knowledge quiz")
+
