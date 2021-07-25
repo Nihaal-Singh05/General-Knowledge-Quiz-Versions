8aa32061d27cc2106803a1004270d6dff8931cca
Nihaal Singh <78886474+Nihaal-Singh05@users.noreply.github.com>
2021-07-26 10:48:08 +1200

    Update v3end_of_game_summary.py

--- V3/v3end_of_game_summary.py
+++ V3/v3end_of_game_summary.py
@@ -1,3 +1,17 @@
-print("\nCongratulations on completing the General Knowledge Quiz {}!".format(name))
-print("Your final score is",score)
-print("Thank you for playing")
+print("  ")
+
+print("******************** Quiz Summary ********************")
+
+print("Thank you for playing", name)
+
+print("Your final score is", score, "out of",total)
+
+print("Your score as a percentage is", (round(score/total*100,2)),"% of the questions correctly")
+
+print("You got", correct, "questions correct")
+
+print("You got", incorrect, "questions incorrect")
+
+exit()
+      
+
