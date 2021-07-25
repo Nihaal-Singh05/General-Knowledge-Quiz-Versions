9b74bbafbd007039a2daa1c37023630be6a147d1
Nihaal Singh <78886474+Nihaal-Singh05@users.noreply.github.com>
2021-07-26 10:30:55 +1200

    Delete v1asking_question.py

--- v1asking_question.py
+++
@@ -1,9 +0,0 @@
-#this program will ask the user questions
-#the set of questions being asked
-
-score=0
-
-#question 1
-
-print("\nQuestion: 1 | score: {}".format (score))
-ans=input("What is the smallest country in the world? \na.Monaco\nb.Vatican\nc.San Marino\nd.Your answer is: : ")
