46019dde3d82e6eae46cb593b3ab2ecd15850377
Nihaal Singh <78886474+Nihaal-Singh05@users.noreply.github.com>
2021-07-26 10:33:57 +1200

    Delete v3_user_ready_status.py

--- v3_user_ready_status.py
+++
@@ -1,7 +0,0 @@
-#this program is see if the user is ready to use the program
-#what if the user is ready
-if status == 'Yes' or status == 'yes' or status == 'Y' or status == 'y' or status == 'A' or status == 'a':
-    print("Welcome to the General Knowledge quiz.")
-else:
-    print("Thank you trying this quiz ")
-    exit()
