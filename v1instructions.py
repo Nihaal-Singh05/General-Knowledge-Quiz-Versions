949c07a7ed9d431f5f68647431c9801b09f35baa
Nihaal Singh <78886474+Nihaal-Singh05@users.noreply.github.com>
2021-07-26 10:31:06 +1200

    Delete v1instructions.py

--- v1instructions.py
+++
@@ -1,5 +0,0 @@
-#this program is for instructions on the quiz
-
-print("This quiz will ask you questions on general knowledge")
-print("Each question will have four possible answers")
-print("However, there will only be one correct answer per question")
